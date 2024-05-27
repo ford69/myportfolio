26d5bff81a7bd1df4dd893ab5d82657687774c48
Clifford Manu <clifford@myzeepay.com>
2024-05-27 11:37:03 +0000

    remove outdist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            outDir: 'dist',
             refresh: true,
         }),
     ],
